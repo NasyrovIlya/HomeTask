0bbdbd4e0e8749ae88bf86b890d8745176f4191c
NasyrovIlya <2loop4uk@list.ru>
2021-10-07 15:55:32 +0500

    edit all  function

--- JS/main.js
+++ JS/main.js
@@ -8,7 +8,8 @@
 // Далее, вывести результат запроса. (успешный или не успешный)
 // Следующая кнопка отрисовать данные в красивом виде.
 // Так же предусмотреть обработку ошибок от сервера.
-// Сделать отрисовку кнопок по всем валютам, по нажатии на одну из них, отрисовывать инфу именно по необходимой валюте.
+// Сделать отрисовку кнопок по всем валютам, по нажатии на одну из них,
+// отрисовывать инфу именно по необходимой валюте.
 
 const URLDATA = 'https://www.cbr-xml-daily.ru/daily_json.js';
 let obj = {};
@@ -17,38 +18,61 @@ async function getData() {
   try {
     const responce = await fetch(URLDATA);
     return await responce.json();
-  } catch (e) {
-    alert(`Произошла ошибка: ${e.message}`);
   }
+  catch (error) {
+    showPopupMessage(`Произошла ошибка при получении данных: ${error.message}`);
+  }
+  return null;
 }
 
 document.addEventListener('click', async (event) => {
   if (event.target.closest('.nav__link')) {
     clearValute();
     obj = await getData();
-    const containerMain = document.querySelector('main');
-    Object.values(obj.Valute).forEach(item => { containerMain.insertAdjacentHTML('beforeEnd', printValute(item)) })
+    printValute();
   }
 
   if (event.target.closest('.btn') && event.target.hasAttribute('id')) {
-    const filterObj = Object.values(obj.Valute).filter(item => {
-      if (item.ID === event.target.getAttribute('id')) { return item; }
-    });
-    ShowPopup(filterObj[0]);
+    showPopup(filterObject(event.target)[0]);
   }
 
   if (event.target.closest('.popup__close')) {
     event.preventDefault();
-    ClosePopup();
+    closePopup();
   }
 })
 
-function ShowPopup(object) {
+function filterObject(inputId) {
+  return Object.values(obj.Valute).filter(item => {
+    if (item.ID === inputId.getAttribute('id')) { return item; }
+  });
+}
+
+function printValute() {
+  try {
+    const containerMain = document.querySelector('main');
+    Object.values(obj.Valute).forEach(item => { containerMain.insertAdjacentHTML('beforeEnd', printValuteItem(item)) })
+  } catch (error) {
+    showPopupMessage(`Произошла ошибка при обработке данных: ${error.message}`);
+  }
+
+}
+
+function showPopupMessage(message) {
+  let messageBody = `
+    <div id = "pupup__valute">
+      <p>${message}</p>
+    </div>`;
+  document.querySelector('.popup__content').insertAdjacentHTML('beforeend', messageBody);
+  document.getElementById('popup').classList.add('open');
+}
+
+function showPopup(object) {
   document.getElementById('popup').classList.add('open');
   document.querySelector('.popup__content').insertAdjacentHTML('beforeend', prinyValuteElement(object));
 }
 
-function ClosePopup() {
+function closePopup() {
   document.getElementById('popup').classList.remove('open');
   document.getElementById('pupup__valute').remove();
 }
@@ -68,7 +92,7 @@ function prinyValuteElement(item) {
   `;
 }
 
-function printValute(item) {
+function printValuteItem(item) {
   return `
     <div class = "main__wrapper">
       <div class = "main__item">${item.CharCode}</div>
